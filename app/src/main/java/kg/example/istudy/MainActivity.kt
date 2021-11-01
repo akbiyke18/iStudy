7f2c0082ab70a800c00021d994d6cea4e1a02b31
админ <dzhoroeva_a@auca,kg>
2021-11-01 14:55:12 +0600

    commit

---
+++ app/src/main/java/kg/example/istudy/MainActivity.kt
@@ -0,0 +1,11 @@
+package kg.example.istudy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
